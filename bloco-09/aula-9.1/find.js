e91fc0e94759c5967005a71aa338892d6c89142a
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2020-05-11 13:45:02 -0300

    Revert "Merge pull request #1 from tryber/aula-9.1"
    This reverts commit ef66a9453bae5cde954af07888986b2bd85d1d3f, reversing
    changes made to 05d9cec8e3f1d0fbe984ab3b269eaff39544c356.

--- bloco-09/aula-9.1/find.js
+++
@@ -1,5 +0,0 @@
-// Higher Order Functions = .find()
-
-// A função find é igual à filter, com uma única diferença: 
-// ao invés de retornar todos os elementos que satisfazem a condição dada como parâmetro, 
-// ela retorna o primeiro elemento que se enquadra.
