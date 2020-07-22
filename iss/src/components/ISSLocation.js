8511199ef05385cb57dd63f1d19332cb770f9a75
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2020-07-22 18:19:10 -0300

    Codigo aula ao vivo

--- iss/src/components/ISSLocation.js
+++ iss/src/components/ISSLocation.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { fetchISSLocation } from '../actions';
+import ISSContext from '../context/ISSContext';
 
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker';
 
 class ISSLocation extends Component {
   componentDidMount() {
-    const { getCurrentISSLocation } = this.props;
+    const { fetchApi } = this.context;
 
     this.timer = setInterval(
-      getCurrentISSLocation,
-      50
+      fetchApi,
+      1000
     );
   }
 
-  componentWillMount() {
+  componentWillUnmount() {
     clearInterval(this.timer);
   }
 
@@ -24,10 +25,9 @@ class ISSLocation extends Component {
       isFetching,
       latitude,
       longitude
-    } = this.props;
+    } = this.context;
 
     const isLocationPresent = latitude && longitude;
-
     return (
       <div>
         <div className="map">
@@ -36,28 +36,18 @@ class ISSLocation extends Component {
             defaultWidth={700}
             height={450}
             minZoom={1}
-            maxZoom={8}
+            maxZoom={20}
             zoom={1}
           >
             {isLocationPresent && <Marker anchor={[latitude, longitude]} />}
           </Map>
         </div>
-        {/* {isFetching && 'Loading...'} */}
+        {isFetching && 'Loading...'}
       </div>
-    );
+    )
   }
 }
 
-const mapStateToProps = (state) => (
-  {
-    isFetching: state.issLocation.isFetching,
-    latitude: state.issLocation.latitude,
-    longitude: state.issLocation.longitude,
-  }
-);
+ISSLocation.contextType = ISSContext;
 
-const mapDispatchToProps = (dispatch) => ({
-  getCurrentISSLocation: () => dispatch(fetchISSLocation()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ISSLocation);
+export default ISSLocation;
