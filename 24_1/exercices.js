5439cbc3f963d39600e1a076e18dd03bcd8eb0ff
Renato Filho <renatosousafilho@gmail.com>
2020-09-09 15:59:12 -0300

    aula 24.1

---
+++ 24_1/exercices.js
@@ -0,0 +1,8 @@
+// Exercício 14: Remova o campo class dos documentos que possuem esse campo com o valor unknown.
+
+// Exercício 15: Produza uma query que renomeie os campos de name para hero_name, e de true_name para full_name e adicione o campo power com valor 100, em todos os documentos.
+
+// Exercício 16: Produza uma query onde, os mutantes class omega ou gama passam a ter seu poder de 500 somente se seu poder for menor que 500.
+
+// Exercício 19: Em apenas uma query adicione o campo area com o seguinte array como valor: [ "Students Room" ] a todos os mutantes que são Senior Staff que tenham poder acima de 100 e para todos os Junior Staff com poder acima de 200.
+
