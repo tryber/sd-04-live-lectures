7b08ada3fec9badd2d176989a1226ce9bc6ac15c
Renato Filho <renatosousafilho@gmail.com>
2020-09-15 16:14:14 -0300

    aula 25_1 - Aggregation Framework - Parte 1

---
+++ 25_1/exercicios.js
@@ -0,0 +1,30 @@
+// 1. Adicionar um campo rating na colecao friends com um valor entre 0 e 10 (utilize a função Math.random)
+
+// 2. Adicionar um campo watches na colecao friends com um valor entre 0 e 10.000.000
+
+// 3. trazer a média de rating dos episódios da primeira a quinta temporada ordernado pela avaliacao de forma decresecnte
+// { temporada: <valor>,  avaliacao: <valor>}
+
+// 4. retornar a temporada com mais reproducoes (watching) da sexta temporada a décima temporada no formato:
+// { temporada: <valor>,  reproducoes: <valor>}
+
+// 5. Adicionar o campo attack e defense na coleção pokemon com valores aleatorios entre 1 e 100.
+
+// 6. Calcular a média, o maior e menor poder de ataque de cada tipo no seguinte formato ordenado pelo ataque de forma crescente.
+// { tipo: <valor>,  mediaAtaque: <valor>, menorAtaque: <valor>, maiorAtaque: <valor>}
+
+// 7. Calcular a média, o maior e menor poder de defesa de cada fraqueza no seguinte formato ordenado pela maior ataque, menor ataque e media de forma decrescente.
+// { fraqueza: <valor>,  mediaDefesa: <valor>, menorDefesa: <valor>, maiorDefesa: <valor>}
+
+// 8. Descubra o total de pokemons que o poder de defesa é maior que o poder de ataque. 
+
+// 9. Encontre o total de pedidos que possuam nos itens comprados um produto da categoria Bebidas
+
+// 10. Descubra quantos pedidos existem para cada categoria de produto.
+
+// Bônus
+
+// 11. Descubra o pedido com o maior valor na categoria Higiene pessoal
+
+// 12. Listar os 5 produtos mais vendidos na categoria Alimentação
+
