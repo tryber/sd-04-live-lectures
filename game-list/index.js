7e1f800fa6369f5b63218d5fe39158d7894f7574
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-10-10 15:50:11 -0300

    alterando index.js rotas e pasta static

--- game-list/index.js
+++ game-list/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const middlewares = require('./middlewares/auth');
+const gameController = require('./controllers/gameController');
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public/`)); //pasta dos nossos arq .js e .css
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.get(
+  '/updateForm/:id', 
+  middlewares.authMid, 
+  gameController.updateViewForm
+);
+
+app.post(
+  '/updateGame/:id', 
+  gameController.update
+);
 
 app.listen(3000, () => console.log('Escutando !!!'));
