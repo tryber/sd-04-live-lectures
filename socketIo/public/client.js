150a9ff5fd3915eea3f565aa1f5676ea774ec82c
vinicius-vasconcelos <viniciussouzav@gmail.com>
2021-01-23 16:03:09 -0300

    Modificando client.js - recebe nome e lista

--- socketIo/public/client.js
+++ socketIo/public/client.js
@@ -1,4 +1,22 @@
 window.onload = () => {
   const clientSocketIo = window.io('http://localhost:3000');
+
+  clientSocketIo.on('myNickname', (name) => {
+    const divUsers = document.getElementById('users')
+    const li = document.createElement('li')
+      li.setAttribute('data-name', 'user-online')
+    li.textContent = name;
+
+    divUsers.append(li)
+  });
+
+  clientSocketIo.on('newUser', (username) => {
+    const divUsers = document.getElementById('users')
+    const li = document.createElement('li')
+      li.setAttribute('data-name', 'user-online')
+    li.textContent = username;
+
+    divUsers.append(li)
+  });
   
 }
