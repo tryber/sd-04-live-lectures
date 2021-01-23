4fa2d6c1b3de8b487e62664cec63fef73e057280
vinicius-vasconcelos <viniciussouzav@gmail.com>
2021-01-23 16:01:46 -0300

    Modificando server.js - envio de nome aleatório

--- socketIo/server.js
+++ socketIo/server.js
@@ -10,6 +10,15 @@ const io = socketIo(httpServer);
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-io.on('connection', (socket) => { });
+io.on('connection', (socket) => {
+  // Criando nome aleatório e injetando dentro da instância do socket
+  socket.user = { nickname: faker.name.firstName() };
+
+  // Explicado a diferença durate a revisão 
+  socket.emit('myNickname', socket.user.nickname)
+  socket.broadcast.emit('newUser', socket.user.nickname);
+  //io.emit('newUser', socket.user.nickname);
+
+});
 
 httpServer.listen(3000, () => console.log('RUN SERVER 127.0.0.1:3000'));
